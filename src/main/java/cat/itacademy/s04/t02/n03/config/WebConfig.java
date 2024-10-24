dc7d0bd582f680be3d4f3f1c7b2c7e3c39e1af70
isma <elsantodeplata@gmail.com>
2024-10-24 11:53:30 +0200

    Añadida clase WebConfig para que la url no sea case sensitive

---
+++ src/main/java/cat/itacademy/s04/t02/n03/config/WebConfig.java
@@ -0,0 +1,18 @@
+package cat.itacademy.s04.t02.n03.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        PathPatternParser patternParser = new PathPatternParser();
+        patternParser.setCaseSensitive(false);
+        
+        configurer.setPatternParser(patternParser);
+    }
+}
